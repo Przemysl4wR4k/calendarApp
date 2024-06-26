12c0b80345e12baa8f208da18ecf26f40e7c6b69
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-06-26 17:38:49 +0200

    basic routing

--- calendar-app/src/app/app.component.ts
+++ calendar-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- calendar-app/src/app/app.routes.ts
+++ calendar-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/calendar', pathMatch: 'full' },
+  { path: 'calendar', loadComponent: () => import('./calendar/calendar.component').then(m => m.CalendarComponent) },
+  { path: 'add-appointment', loadComponent: () => import('./appointment-form/appointment-form.component').then(m => m.AppointmentFormComponent) }
+];
